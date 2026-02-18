6353adb38d6dda1f66c34001490f3ab2523eeb0b
cgomezh <carlos.gomez.h@uni.pe>
2026-02-18 00:06:01 -0500

    2DA ENTREGA
    Correcion de una funcion , siguiendo la estructura solicitada en el ejercicio ; ademas se simplifico y reescribio secciones del codigo y ahora en la liberacion de la memoria hacemos que apunte luego a nullptr como buena practica

--- PC4(FUNDA)/ejercicio04.cpp
+++ PC4(FUNDA)/ejercicio04.cpp
@@ -1,106 +1,122 @@
 #include <iostream>
 #include <cstring>
-using namespace std;
+using namespace std ;
 
 struct Item{
-    char *descripcion;   // memoria dinámica
-    int cantidad;
-    double precioUnitario;
+    char *descripcion ;   // memoria dinámica
+    int cantidad ;
+    double precioUnitario ;
 };
 
 struct Pedido{
-    int numeroPedido;
-    char *nombreCliente; // memoria dinámica
-    Item *items;         // arreglo dinámico de items
-    int cantidadItems;
+    int numeroPedido ;
+    char *nombreCliente ; // memoria dinámica
+    Item *items ;         // arreglo dinámico de items
+    int cantidadItems ;
 };
 
-Item* crearItem(int cantidadItems){
-    Item *b = new Item[cantidadItems];
-    const char *descripciones[] = {"Laptop", "Mouse", "Teclado"}; //Inicializamos de forma dinamica para no pedirlo por teclado
+// FUNCION PARA CREAR EL ITEM
+Item crearItem(const char* descripcion, int cantidad, double precio){
+    Item a ;
 
-    for(int i = 0; i < cantidadItems; i++){
-        //COPIAMOS DESCRI PARA QUE AHORA SEA DINAMICO
-        int n = strlen(descripciones[i]);
-        b[i].descripcion = new char[n + 1];
-        strcpy(b[i].descripcion, descripciones[i]);
+    int n = strlen(descripcion) ; //mide la cantidad de letras para poder asignar en el tamaño de la memoria dinamica
+    a.descripcion = new char[n + 1];
+    strcpy(a.descripcion, descripcion) ; //copia la descripcion
 
-        b[i].cantidad = i + 1;
-        b[i].precioUnitario = 100 * (i + 1);
-    }
+    a.cantidad = cantidad ;
+    a.precioUnitario = precio ;
 
-    return b;
+    return a ;
 }
 
-double calcularTotal(const Pedido *p){
-    double total = 0;
+Pedido *crearPedido(int numero, const char* cliente, int cantidadItems){
+    Pedido *a = new Pedido ;
+
+    a->numeroPedido = numero ;
+    a->cantidadItems = cantidadItems ;
+
+    // GUARDAMOS AHORA EL NOMBRE EN FORMA DINAMICA
+    int n = strlen(cliente) ; //cuenta la cantidad de letras para asiganrlo luego crear la memoria dinamica con espaio suficiente
+    a->nombreCliente = new char[n + 1] ;
+    strcpy(a->nombreCliente, cliente) ;
+
+    // reservamos memoria dinaica
+    a->items = new Item[cantidadItems] ;
+
+    // inicializamos items llamando a crearItem (como pedía el ejercicio)
+    const char *descripciones[] = {"Laptop", "Mouse", "Teclado"} ;
+
+    for(int i = 0; i < cantidadItems; i++){
+        a->items[i] = crearItem(descripciones[i], i + 1, 100 * (i + 1)) ; //forma simplificada (respecto a lo que presente en la pc)
+    }
+
+    return a ;
+}
+
+double calcularTotal(const Pedido *p){ //CALCULA EL PRECIO TOTAL REAIZANDO UNA SUMATORIA
+    double total = 0 ;
     for(int i = 0; i < p->cantidadItems; i++){
         total += p->items[i].cantidad * p->items[i].precioUnitario;
     }
     return total;
 }
 
-Pedido* crearPedido(int numero, const char *cliente, int cantidadItems){
-    Pedido *a = new Pedido;
-
-    a->numeroPedido = numero;
-    a->cantidadItems = cantidadItems;
-
-    int n = strlen(cliente);
-    a->nombreCliente = new char[n + 1]; //para hacerlo de forma dinamica
-    strcpy(a->nombreCliente, cliente); // copiamos
-
-    a->items = crearItem(cantidadItems);
-
-    return a;
-}
-
-Item* itemMasCaro(const Pedido *p){
-    int indice = 0;
+//MEDIANTE COMPARACION SIMPLE ENCONTRAMOS EL ITEM MAS CARO
+Item *itemMasCaro(const Pedido *p){
+    int index = 0;
     double mascaro = p->items[0].precioUnitario;
+
     for(int i = 1; i < p->cantidadItems; i++){
         if(p->items[i].precioUnitario > mascaro){
             mascaro = p->items[i].precioUnitario;
-            indice = i;
+            index = i;
         }
     }
-    return &p->items[indice];
+
+    return &p->items[index];
 }
 
-void liberarPedido(Pedido *p){// ELIMNAMOS POR PARTES YA QUE TENEMOS ESTRUCTURAS CON ARREGLOS (PUNTEROS)
-    for(int i = 0; i < p->cantidadItems; i++){
-        delete[] p->items[i].descripcion;
+void liberarPedido(Pedido *p){
+    // AQUI LIBERAMOS LA MEMORIA PARA CADA DESCRIPCION YA QUE ESTE FUE CREADA DE FORMA DINAMICA
+    for(int i = 0 ; i < p->cantidadItems ; i++){
+        delete[] p->items[i].descripcion ; //eliminamos cada descri
     }
-    delete[] p->items;
-    delete[] p->nombreCliente;
-    delete p;
+
+    delete[] p->items ; //eliminamos el espacio que opcuapaba
+    delete[] p->nombreCliente ;
+    delete p ;
+
 }
 
 int main(){
-    int numero = 1;
-    char cliente[30] = "Carlos Perez"; //luego lo hacemos dinamico en la funcion
-    int cantidadItems = 3;
+    int numero = 1 ;
+    char cliente[30] = "Carlos Perez" ;
+    int cantidadItems = 3 ;
 
-    Pedido *A = crearPedido(numero, cliente, cantidadItems);
+    Pedido *A = crearPedido(numero, cliente, cantidadItems) ;
 
-    cout << "Pedido N°: " << A->numeroPedido << endl;
-    cout << "Cliente: " << A->nombreCliente << endl;
+    cout << "Pedido N°: " << A->numeroPedido << endl ;
+    cout << "Cliente: " << A->nombreCliente << endl ;
 
+    //IMPRIMIMOS
     cout << "Items:" << endl;
     for(int i = 0; i < A->cantidadItems; i++){
         cout << " - " << A->items[i].descripcion
              << " | Cant: " << A->items[i].cantidad
-             << " | Precio: " << A->items[i].precioUnitario << endl;
+             << " | Precio: " << A->items[i].precioUnitario << endl ;
     }
 
-    double total = calcularTotal(A);
-    cout << "Total: " << total << endl;
+    double total = calcularTotal(A) ; //implementacion de la funcion que calcula el total
+    cout << "Total: " << total << endl ;
 
-    Item *caro = itemMasCaro(A);
+    Item *caro = itemMasCaro(A) ; //iplementacion que busca lo mas caro
     cout << "Item mas caro: " << caro->descripcion
-         << " | Precio: " << caro->precioUnitario << endl;
+         << " | Precio: " << caro->precioUnitario << endl ;
 
-    liberarPedido(A);
+    liberarPedido(A) ;
+    //Y COMO BUENA PRACTICA HACEMOS QUE APUNTE A nullptr
+    A = nullptr ;
 
-    return 0;
+    return 0 ;
 }
+
